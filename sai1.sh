a6296ac60147bdcc5016f2163cfc38a2e7b89173
root <root@localhost.localdomain>
2017-10-18 15:30:22 +0530

    for cloning sai1

---
+++ sai1.sh
@@ -0,0 +1,15 @@
+hvhv,jh..j.j
+
+jhjk;huihjjknkn/n'likhjkljnml
+jbdjfbsuhfdb hvbsdhfyut 
+jasbbuusudf ygjsdg ytsjkbd\
+jfiyhfdugsyrbjsbdglajsbdgv ]\
+kahs_+ugs 
+ugsryk
+146bsdyu
+jhsf7
+jhdf
+hdsflasjfals
+sai venkata sai kumar
+kotni venkatsai kumar
+kotni venkatsai kumar
